128a93fddb0d3079ed47056b37bcd2d0ec03c828
DCluseau <daphne.cluseau@fms-ea.com>
2026-04-02 09:37:07 +0200

    Added Articles list by category + template to update an article + category list in add article function

--- src/main/java/fr/fms/dao/CategoryRepository.java
+++ src/main/java/fr/fms/dao/CategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.fms.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+	public Category findByName(String name);
 }
